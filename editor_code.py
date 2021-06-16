3860ffd70ae644096004bf7151d2f4557fe49e2b
Peeyush Sanam <peeyush.sanam@gmail.com>
2021-06-16 23:49:28 +0530

    Add files via upload

---
+++ editor_code.py
@@ -0,0 +1,53 @@
+##a= 10
+##while a>0:
+##    print('hello')
+##    a -= 2
+##    if a>3 and a<7:
+##        a = 1
+
+##s = 'hello'
+##for i in s:
+##    print('Good', i)
+
+##for i in range(9):
+##    print('good')
+
+##for i in range(10,101,20):
+##    print(i)
+
+a = [45,7,8,9,10]
+b = []
+for i in a:
+    b.append(i*2)
+print(a)
+print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ shell_code.py
@@ -0,0 +1,113 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Looping Statements
+>>> # 1. While
+>>> # 2. For
+>>> 
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+hello
+hello
+hello
+hello
+hello
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+hello
+hello
+hello
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+Good
+Good
+Good
+Good
+Good
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+Good h
+Good e
+Good l
+Good l
+Good o
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+good
+good
+good
+good
+good
+good
+good
+good
+good
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+50
+52
+54
+56
+58
+60
+62
+64
+66
+68
+70
+72
+74
+76
+78
+80
+82
+84
+86
+88
+90
+92
+94
+96
+98
+100
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+10
+30
+50
+70
+90
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+[45, 7, 8, 9, 10, 45, 7, 8, 9, 10]
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+90
+14
+16
+18
+20
+>>> 
+========== RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day7/editor_code.py ==========
+[45, 7, 8, 9, 10]
+[90, 14, 16, 18, 20]
+>>> 
